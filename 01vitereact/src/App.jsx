09c18eb330c24a797e0dc8c6289490c4f8bf1db3
Anmolkumar2002 <anmolkumar200210@gmail.com>
2024-12-14 13:22:47 +0530

    vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  const userName = "chai or code"
+  return (
+    <>
+    <h1>Chai aur React with Vite | AKM@ {userName}</h1>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import React from 'react';
+
+import App from './App.jsx'
+
+function Myapp(){
+    return (
+        <div>
+            <h1>Custom App ! | Akp</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit on google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherUser
+)
+
+createRoot(document.getElementById('root')).render(
+  
+    // <Myapp/>
+    // anotherElement
+    reactElement
+    // <App/>
+  
+)
